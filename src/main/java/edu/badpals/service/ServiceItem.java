e4108704e7ea50f0fed9a6ab98fe092091fa795c
bestiadivina147 <diegopc147@gmail.com>
2024-06-06 11:04:13 +0200

    build(ServiceItem.java): add class ServiceItem.java

---
+++ src/main/java/edu/badpals/service/ServiceItem.java
@@ -0,0 +1,40 @@
+package edu.badpals.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import edu.badpals.domain.MagicalItem;
+import edu.badpals.repository.Repositorio;
+import jakarta.inject.Inject;
+
+public class ServiceItem implements Servicio {
+
+    @Inject
+    Repositorio repo;
+    @Override
+    public Optional<MagicalItem> cargaItem(String name) {
+        return repo.loadItem(name);
+    }
+
+    @Override
+    public Optional<MagicalItem> cargaItem(MagicalItem item) {
+        return repo.loadItem(item);
+    }
+
+    @Override
+    public List<MagicalItem> cargaItems(String name) {
+        return repo.loadItems(name);
+    }
+
+    @Override
+    public Optional<MagicalItem> creaItem(MagicalItem item) {
+        repo.createItem(item.getName(), item.getQuality(), item.getType());
+        return repo.loadItem(item);
+    }
+
+    @Override
+    public void eliminaItem(MagicalItem item) {
+        repo.deleteItem(item);
+    }
+    
+}
