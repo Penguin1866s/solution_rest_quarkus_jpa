1e103550add930e3ec9b1380381ff4bc792b6c42
bestiadivina147 <diegopc147@gmail.com>
2024-06-06 10:58:11 +0200

    build(Servicio.java): add class Servicio.java

---
+++ src/main/java/edu/badpals/service/Servicio.java
@@ -0,0 +1,15 @@
+package edu.badpals.service;
+
+import java.util.Optional;
+import java.util.List;
+
+import edu.badpals.domain.MagicalItem;
+
+public interface Servicio {
+    Optional<MagicalItem> cargaItem(String name);
+    Optional<MagicalItem> cargaItem(MagicalItem item);
+    List<MagicalItem> cargaItems(String name);
+    Optional<MagicalItem> creaItem(MagicalItem item);
+    void eliminaItem(MagicalItem item);
+    
+}
